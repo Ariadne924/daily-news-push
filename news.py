f421e8a5ab068d5677dd3d7eb8ddae4c6637493a
Ariadne924 <3161395028@qq.com>
2026-04-21 10:55:11 +0800

    Update news.py

--- news.py
+++ news.py
@@ -2,12 +2,10 @@ import requests
 SEND_KEY = "SCT340534Tzruc1Oeqj2q71uLgXPp34n8s"
 
 def get_news():
-    # 抓取金融早报
-    finance = requests.get("https://api.jinrishici.com/v1/getFinanceNews", timeout=10).json()["data"][:5]
-    # 抓取教育资讯
-    edu = requests.get("https://api.jinrishici.com/v1/getEduNews", timeout=10).json()["data"][:3]
-    # 合并内容
-    news = ["【今日金融早报】"] + [f"{i+1}. {n['title']}" for i,n in enumerate(finance)]
+finance = requests.get("https://api.vvhan.com/api/news?type=finance&page=1", timeout=10).json()["data"][:5]
+  
+edu = requests.get("https://api.vvhan.com/api/news?type=edu&page=1", timeout=10).json()["data"][:3]
+news = ["【今日金融早报】"] + [f"{i+1}. {n['title']}" for i,n in enumerate(finance)]
     news += ["\n【教育资讯更新】"] + [f"{i+1}. {n['title']}" for i,n in enumerate(edu)]
     return "\n".join(news)
 
