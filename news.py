304168fe6c091c8c1dc4a76ff5de30ad6f9b745e
Ariadne924 <3161395028@qq.com>
2026-04-21 11:03:40 +0800

    Update news.py

--- news.py
+++ news.py
@@ -2,9 +2,9 @@ import requests
 SEND_KEY = "SCT340534Tzruc1Oeqj2q71uLgXPp34n8s"
 
 def get_news():
-    finance = requests.get("https://api.vvhan.com/api/news?type=finance&page=1", timeout=10).json()["data"][:5]
-  
-    edu = requests.get("https://api.vvhan.com/api/news?type=edu&page=1", timeout=10).json()["data"][:3]
+    finance = requests.get("https://www.cls.cn/api/sw?app=CailianpressWeb&os=web&sv=7.7.5&action=getRollingNews&lastTime=0&lastId=0&pageSize=5&channel=finance", timeout=10).json()["data"]["roll_news"][:5]
+    
+    edu = requests.get("https://www.cls.cn/api/sw?app=CailianpressWeb&os=web&sv=7.7.5&action=getRollingNews&lastTime=0&lastId=0&pageSize=3&channel=education", timeout=10).json()["data"]["roll_news"][:3]
     news = ["【今日金融早报】"] + [f"{i+1}. {n['title']}" for i,n in enumerate(finance)]
     news += ["\n【教育资讯更新】"] + [f"{i+1}. {n['title']}" for i,n in enumerate(edu)]
     return "\n".join(news)
